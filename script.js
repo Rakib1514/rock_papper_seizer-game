3337ff676c542490c83cef51494429e247ae643d
rakib1514 <rkrakib1514@outlook.com>
2024-09-10 02:16:51 +0600

    final

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 let userScore = 0;
 let compScore = 0;
 
+// DOM (Selector)
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
-const userScorePara = document.querySelector('#user-score');
-const compScorePara = document.querySelector('#comp-score');
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
+// Computer Random number select from 0-3. then select idx on arry
 const genCompChoice = () => {
   const option = ["rock", "paper", "scissors"];
   const randIdx = Math.floor(Math.random() * 3);
   return option[randIdx];
 };
 
+// Draw game massage
 const drawGame = () => {
-  msg.innerText = "Game was Draw."
+  msg.innerText = "Game was Draw.";
   msg.style.backgroundColor = "#081b31";
-
 };
 
-let showWinner = (userWin,userChoice,compChoice) => {
+// Show on Html or webPage
+let showWinner = (userWin, userChoice, compChoice) => {
   if (userWin) {
-    userScore++
+    userScore++;
     userScorePara.innerText = userScore;
     msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
     msg.style.backgroundColor = "green";
   } else {
-   compScore++
-   compScorePara.innerText = compScore;
-    msg.innerText =  `You lose. ${compChoice} beats your ${userChoice}`;
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lose. ${compChoice} beats your ${userChoice}`;
     msg.style.backgroundColor = "red";
   }
 };
 
+// Game logic on conditional
 const playGame = (userChoice) => {
-  
   const compChoice = genCompChoice();
 
   if (userChoice === compChoice) {
@@ -51,10 +54,11 @@ const playGame = (userChoice) => {
       //rock, paper
       userWin = compChoice === "rock" ? false : true;
     }
-    showWinner(userWin,userChoice,compChoice);
+    showWinner(userWin, userChoice, compChoice);
   }
 };
 
+// Event listener on click
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
